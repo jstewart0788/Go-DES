24214bc897c803b7ea8be6243ccf6c27c3ddf9a8
Jonathan Stewart <jstewart0788@gmail.com>
2020-10-21 18:38:43 -0400

    finished with assignment

--- main.go
+++ main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	var cypherText cypherText
 	var decodedText string
-	// reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
 	// Get input from user
-	// fmt.Print("Enter your message: ")
-	// message, _ := reader.ReadString('\n')
-	// message = removeCarriageWindows(message)
-	// fmt.Print("Enter your key: ")
-	// key, _ := reader.ReadString('\n')
-	// key = removeCarriageWindows(key)
-	message := "Okay this is a grand opportunity to test this out"
-	key := "ABCDEFGH"
+	fmt.Print("Enter your message: ")
+	message, _ := reader.ReadString('\n')
+	message = removeCarriageWindows(message)
+	fmt.Print("Enter your key(length 8):")
+	key, _ := reader.ReadString('\n')
+	key = removeCarriageWindows(key)
+	if len(key) != 8 {
+		fmt.Println("Key is incorrect length. Please enter an eight chracter key")
+		os.Exit(1)
+	}
+	// message := "Okay this is a grand opportunity to test this out"
+	// key := "ABCDEFGH"
 
 	chunks := stringToEightByteArray(message)
 	for _, chunk := range chunks {
